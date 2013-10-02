4e529ad5df525991cade211cbd81c2a98d9f1d3d
brlittle <brlittle@gmail.com>
2013-10-02 10:49:31 -0400

    bot source

---
+++ bot.py
@@ -0,0 +1,40 @@
+import sys
+import twitter, os, time, random
+
+api = twitter.Api(consumer_key='tx96cAqBxlmzLSmIyfLO4w',
+                  consumer_secret='9hVpmjvjsmacV73tuAYPwqBer8NPUOpRGWmh4Ay1VY',
+                  access_token_key='1922646931-6CWDP4cYUdWvL6E7XrtYzbnQM8sgWM684qIWif3',
+                  access_token_secret='zYOAtmM1ouVE9M95ybSe5Z3seKgknd6N6PWJVCc')
+
+# open file in read mode
+file = open("bond.txt", "r")
+contentbond=[]
+
+# read file line by line and store lines to a list
+for line in file:
+	contentbond.append(line)
+
+# open file in read mode
+file = open("eyre.txt", "r")
+contenteyre=[]
+
+# read file line by line and store lines to a list
+for line in file:
+	contenteyre.append(line)
+
+# select a tweet at random and cut of the \n in the end
+tweet = (random.choice(contentbond)[:-1]) + (random.choice(contenteyre)[:-1])
+print tweet
+if len(tweet) < 130:
+    api.PostUpdate(tweet)
+else:
+    numtweets = int(round((len(tweet)/130)+.5))
+    curtweet = 1
+    startcut = 0
+    endcut = 130
+    for i in range(1,numtweets+1):
+        newtweet = tweet[startcut:endcut]
+        api.PostUpdate('(' + str(curtweet) + '/' + str(numtweets) + ') ' + newtweet)
+        curtweet = curtweet + 1
+        startcut = startcut + 130
+        endcut = endcut + 130
